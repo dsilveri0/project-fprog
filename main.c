3bf9d84311621b61b4cac25622a0dce404b94b46
David Silverio <david.a.silverio@gmail.com>
2019-12-18 00:40:53 +0000

    Got the first running version of the project. I'm using 4 separated structures to hold my data, might joined them in one big structure of structures in the near future. Right now the main menu is doing the submenu's work but I'll have to change that aswell. Reading and writing to binary files not implemented yet.

--- main.c
+++ main.c
@@ -2,11 +2,10 @@
 ###################################################
 |                                                 |
 |      Projeto de Fundamentos de Programação      |
-|                by David Silvério                |
+|       de David Silvério e Vladyslav Bobko       |
 |                                                 |
 ###################################################
 **************************************************/
-
 
 #include <stdio.h>
 #include <stdlib.h>
@@ -18,77 +17,75 @@
 #define NUM_MAX_CUSTOS 200
 
 typedef struct {
-    typedef struct {
-        int id_conta;
-        char designacao_conta[50];
-        char plataforma_fornecedor_servicos[50];
-        char organizacao[50];
-        char dominio[50];
-        float saldo_conta;
-    } conta;
+    int id_conta;
+    char designacao_conta[30];
+    char plataforma_fornecedor_servicos[30];
+    char organizacao[30];
+    char dominio[30];
+    float saldo_conta;
+} conta;
 
-    typedef struct {
-        int id_projeto;
-        dados dados.conta.id_conta;
-        char nome_projeto[50];
-        char equipa_projeto[50];
-        float data_projeto;
-    } projeto;
+typedef struct {
+    int id_projeto;
+    conta id_da_conta;
+    char nome_projeto[30];
+    char equipa_projeto[30];
+    float data_projeto;
+} projeto;
 
-    typedef struct {
-        int id_servico;
-        char designacao_servico[50];
-        char tipo_servico[50];
-        char unidade_medida[20];
-        float custo_unidade;
-    } servico;
+typedef struct {
+    int id_servico;
+    char designacao_servico[30];
+    char tipo_servico[30];
+    char unidade_medida[30];
+    float custo_unidade;
+} servico;
 
-    typedef struct {
-        dados dados.servico.id_servico;
-        dados dados.projeto.id_projeto;
-        float datahora_inicio_utilizacao;
-        float datahora_fim_utilizacao;
-        float quantidade;
-        float valor_pago;
-    } custo;
-} dados;
+typedef struct {
+    servico id_do_servico;
+    projeto id_do_projeto;
+    float datahora_inicio_utilizacao;
+    float datahora_fim_utilizacao;
+    float quantidade;
+    float valor_pago;
+} custo;
 
 int ler_numero(int, int);
 char menu_principal(void);
-char sub_menu(char);
+//char sub_menu(char);
 int confirmar_saida(void);
 int voltar_atras(void);
 
-void gravar_dados_ficheiro(dados[], int);
-int ler_dados_ficheiro(dados[]);
+//void gravar_dados_ficheiro(dados[], int);
+//int ler_dados_ficheiro(dados[]);
 
-void mostrar_dados(dados[], int);
-void ler_dados(dados[], int);
+void mostrar_dados(conta[], int);
+void ler_dados(conta[], int);
 
 int main() {
     int num_contas = 0;
     char op;
 
-    dados dados.conta[NUM_MAX_CONTAS];
-    dados dados.projeto[NUM_MAX_PROJETOS];
-    dados dados.servico[NUM_MAX_SERVICOS];
-    dados dados.custo[NUM_MAX_CUSTOS];
+    conta conta[NUM_MAX_CONTAS];
+    //projeto projeto[NUM_MAX_PROJETOS];
+    //servico servico[NUM_MAX_SERVICOS];
+    //custo custo[NUM_MAX_CUSTOS];
 
     do {
-	    op = menu_opcoes();
+	    op = menu_principal();
 
 	    switch(op) {
             case '1':
                 printf("Contas\n");
 
-                ler_dados(dados.conta, num_contas);
+                ler_dados(conta, num_contas);
                 num_contas++;
 
                 break;
             case '2':
                 printf("\n\tMostrar Contas\n");
 
-                mostrar_dados(dados.conta, num_contas);
+                mostrar_dados(conta, num_contas);
 
                 break;
             case '3':
@@ -125,7 +122,7 @@ int main() {
     return 0;
 }
 
-char menu_opcoes(void) {
+char menu_principal(void) {
 
     char op;
 
@@ -133,10 +130,12 @@ char menu_opcoes(void) {
             printf("\n\t----- Menu de Opcoes -----\n\n");
             printf(" 1 - Registar dados de contas\n");
             printf(" 2 - Mostrar dados de contas\n");
-            /*printf(" 3 - Alterar dados dos estudantes\n");
+            /*
+            printf(" 3 - Alterar dados dos estudantes\n");
             printf(" 4 - Estatisticas\n");
             printf(" 5 - Gravar dados no ficheiro binario\n");
-            printf(" 6 - Ler dados do ficheiro binario\n"); */
+            printf(" 6 - Ler dados do ficheiro binario\n");
+            */
             printf(" 0 - Sair\n");
             printf("\n\tSelecione uma opcao -> ");
             scanf(" %c", &op);
@@ -157,35 +156,35 @@ int ler_numero(int lim_inf, int lim_sup) {
 }
 
 
-void ler_dados(dados d_contas[], int num_contas) {
+void ler_dados(conta d_contas[], int num_contas) {
 
     printf("\nIntroduza o ID de conta: ");
-    d_contas[0].conta.id_conta = ler_numero(1, 5000);
+    d_contas[0].id_conta = ler_numero(1, 5000);
 
     printf("\nIndique a designacao da conta: ");
-    scanf("%s", d_contas[0].conta.designacao_conta);
+    scanf("%s", d_contas[0].designacao_conta);
 
     printf("\nIndique a plataforma: ");
-    scanf("%s", d_contas[0].conta.plataforma_fornecedor_servicos);
+    scanf("%s", d_contas[0].plataforma_fornecedor_servicos);
 
     printf("\nIndique a organizacao: ");
-    scanf("%s", d_contas[0].conta.organizacao);
+    scanf("%s", d_contas[0].organizacao);
 
     printf("\nIndique o dominio: ");
-    scanf("%s", d_contas[0].conta.dominio);
+    scanf("%s", d_contas[0].dominio);
 
     printf("\nIntroduza o saldo de conta: ");
-    d_contas[0].conta.saldo_conta = ler_numero(0, 1000);
+    d_contas[0].saldo_conta = ler_numero(0, 1000000000);
 
 }
 
-void mostrar_dados_estudantes(dados d_contas[], int num_contas) {
-    printf("\nid da conta: %d ", d_contas[0].conta.id_conta);
-    printf("\ndesignacao da conta: %s\n", d_contas[0].conta.designacao_conta);
-    printf("\nplataforma da conta: %s\n", d_contas[0].conta.plataforma_fornecedor_servicos);
-    printf("\norganizacao da conta: %s\n", d_contas[0].conta.organizacao);
-    printf("\ndominio da conta: %s\n", d_contas[0].conta.dominio);
-    printf("\nsaldo da conta: %.2f\n", d_contas[0].conta.saldo_conta);
+void mostrar_dados(conta d_contas[], int num_contas) {
+    printf("\nid da conta: %d ", d_contas[0].id_conta);
+    printf("\ndesignacao da conta: %s\n", d_contas[0].designacao_conta);
+    printf("\nplataforma da conta: %s\n", d_contas[0].plataforma_fornecedor_servicos);
+    printf("\norganizacao da conta: %s\n", d_contas[0].organizacao);
+    printf("\ndominio da conta: %s\n", d_contas[0].dominio);
+    printf("\nsaldo da conta: %.2f\n", d_contas[0].saldo_conta);
 }
 
 int confirmar_saida(void) {
