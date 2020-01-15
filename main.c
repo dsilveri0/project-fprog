baf686167c9ae9eea36d524d3401a93e4a2af8eb
Deep as a Sheep <55530846+vladkk24@users.noreply.github.com>
2020-01-15 21:06:02 +0000

    Add files via upload
    sub_menu projetos e tambem melhorei os menu's

--- main.c
+++ main.c
@@ -26,7 +26,7 @@ typedef struct {
 
 typedef struct {
     int id_projeto;
-    conta id_da_conta;
+    int id_da_conta;
     char nome_projeto[30];
     char equipa_projeto[30];
     float data_projeto;
@@ -53,10 +53,14 @@ float ler_numero(int, int);
 char menu_principal(void);
 char submenu_contas(void);
 char submenu_servicos(void);
+char submenu_projeto(void);
 void ler_dados_conta(conta[], int);
 void mostrar_dados_conta(conta[], int);
 void ler_dados_servico(servico[], int);
 void mostrar_dados_servico(servico[], int);
+void ler_dados_projeto(projeto[],int);
+void mostrar_dados_projeto(projeto[],int);
+int voltar_atras(void);
 char confirmar_saida(void);
 
 int main() {
@@ -66,6 +70,8 @@ int main() {
     int num_contas = 0;
     servico vetor_servico[NUM_MAX_SERVICOS];
     int num_servicos = 0;
+    int num_projetos = 0;
+    projeto vetor_projeto[NUM_MAX_PROJETOS];
 
     do {
         op = menu_principal();
@@ -95,8 +101,7 @@ int main() {
                     break;
                 case '3':
                     printf("\n\tVoltar atras\n");
-                    submenu_conta_op = '0';
-                   
+                    //menu_principal();
                     break;
                 case '0':
                     resposta_submenu_conta = confirmar_saida();
@@ -115,6 +120,43 @@ int main() {
             break;
         case '2':
             printf("\n\tSUB-MENU - PROJETOS\n");
+            char submenu_projeto_op;
+            char resposta_submenu_projeto;
+
+            do {
+                submenu_projeto_op = submenu_projeto();
+
+                switch(submenu_projeto_op) {
+                case '1':
+                    printf("\n\tRegistar projetos\n");
+
+                    ler_dados_projeto(vetor_projeto, num_projetos);
+                    num_projetos++;
+
+                    break;
+                case '2':
+                    printf("\n\tConsultar projetos\n");
+
+                   mostrar_dados_projeto(vetor_projeto,num_projetos);
+
+                    break;
+                case '3':
+                    printf("\n\tVoltar atras\n");
+                    //menu_principal();
+                    break;
+                case '0':
+                    resposta_submenu_projeto = confirmar_saida();
+
+                    if (resposta_submenu_conta == 'S' || resposta_submenu_conta == 's'){
+                        printf("\nA sair... \n");
+                        return 0;
+                    }
+
+                    break;
+                default:
+                    printf("\nIntroduza uma opcao valida!");
+                }
+            } while(submenu_conta_op != '0');
 
             break;
         case '3':
@@ -141,7 +183,6 @@ int main() {
                     break;
                 case '3':
                     printf("\n\tVoltar atras\n");
-                    submenu_servico_op = '0';
 
                     break;
                 case '0':
@@ -187,7 +228,7 @@ int main() {
             printf("Introduza uma opcao valida!");
         }
     } while(op != '0');
-   
+
     return 0;
 }
 
@@ -215,11 +256,13 @@ char submenu_contas(void) {
     char op;
 
     do {
-        printf("\n 1 - Registar conta\n");
-        printf(" 2 - Consultar conta\n");
-        printf(" 3 - Voltar atras\n");
-        printf(" 0 - Sair\n");
-        printf("\n\tSelecione uma opcao -> ");
+            printf("|------- Menu de Opcoes --------|\n");
+            printf("|*******************************|\n");
+            printf("|1 - Registar dados de contas   |\n");
+            printf("|2 - Mostrar dados de contas    |\n");
+            printf("|3 - Voltar atras               |\n");
+            printf("|0 - Sair                       |\n");
+            printf("|------------------------------ |\n");
         scanf(" %c", &op);
     } while(op != '1' && op != '2' && op != '3' && op != '0');
 
@@ -230,15 +273,35 @@ char submenu_servicos(void) {
     char op;
 
     do {
-        printf("\n 1 - Registar servico\n");
-        printf(" 2 - Consultar servicos\n");
-        printf(" 3 - Voltar atras\n");
-        printf(" 0 - Sair\n");
-        printf("\n\tSelecione uma opcao -> ");
+            printf("|-------- Menu de Opcoes --------|\n");
+            printf("|******************************* |\n");
+            printf("|1 - Registar servico            |\n");
+            printf("|2 - Mostrar servicos registrados|\n");
+            printf("|3 - Voltar atras                |\n");
+            printf("|0 - Sair                        |\n");
+            printf("|--------------------------------|\n");
         scanf(" %c", &op);
     } while(op != '1' && op != '2' && op != '3' && op != '0');
 
     return op;
+}
+
+char submenu_projeto(void) {
+    char op;
+
+        do {
+            printf("|------- Menu de Opcoes --------|\n");
+            printf("|*******************************|\n");
+            printf("|1 - Registar dados do projeto  |\n");
+            printf("|2 - Mostrar dados do projeto   |\n");
+            printf("|3 - Voltar atras               |\n");
+            printf("|0 - Sair                       |\n");
+            printf("|------------------------------ |\n");
+            printf("\n\tSelecione uma opcao -> ");
+            scanf(" %c", &op);
+        } while(op != '1' && op != '2' && op != '3' && op != '4' && op != '5' && op != '6' && op != '0');
+
+	return op;
 }
 
 float ler_numero(int lim_inf, int lim_sup) {
@@ -316,6 +379,36 @@ void mostrar_dados_servico(servico s_vetor[], int s_numero) {
         printf("\nTipo de servico: %s", s_vetor[i].tipo_servico);
         printf("\nUnidade de medida: %s", s_vetor[i].unidade_medida);
         printf("\nCusto por unidade: %.2f\n", s_vetor[i].custo_unidade);
+    }
+}
+
+void ler_dados_projeto(projeto p_vetor[], int p_numero){
+
+    int id_conta;
+
+    printf("\nIndique o numero da conta qual o projeto esta associado a: ");
+    id_conta = ler_numero(0,5);
+    p_vetor[p_numero].id_da_conta = id_conta;
+
+    printf("\nIndique o nome do projeto: ");
+    scanf("%s", p_vetor[p_numero].nome_projeto);
+
+    printf("\nIndique a divisao/equipa: ");
+    scanf("%s", p_vetor[p_numero].equipa_projeto);
+
+    printf("\nIndique a data: ");
+    printf("\n*>.> Exemplo 12022020, 12=dia, 02=mes, 2020=ano <.<*");
+   // p_vetor.data_projeto = ler_numero(01012019,32139999); //tipo aqui a data mais pequena e a maior possivel neste ano
+}
+
+void mostrar_dados_projeto(projeto p_vetor[], int p_numero) {
+    for(int i = 0; i<p_numero;i++) {
+        printf("\n\nID do projeto: %d", p_vetor[i].id_projeto);
+        printf("\nConta Associada: %d", p_vetor[i].id_da_conta);
+        printf("\nNome do projeto: %s", p_vetor[i].nome_projeto);
+        printf("\nDivisao/Equipa: %s", p_vetor[i].equipa_projeto);
+        // printf("\nData: %s", p_vetor[i].data_projeto); ignorei para testes
+        printf("\n\n\n");
     }
 }
 
